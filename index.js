8f7fee2933eb51ee62e29e715deeda3f88e2161c
Trygve Lie <trygve.lie@finn.no>
2021-09-26 00:54:40 +0200

    Get paths

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 export function podlet(req, res) {
-    res.send(`Hello podlet! - ${req.originalUrl}`);
+    if (req.originalUrl.startsWith('/content')) {
+      res.send(`Hello content!`);
+      return;
+    }
+    
+    if (req.originalUrl.startsWith('/fallback')) {
+      res.send(`Hello fallback!`);
+      return;
+    }
+
+    if (req.originalUrl.startsWith('/manifest.json')) {
+      res.send(`Hello manifest!`);
+      return;
+    }
+
+    res.status(404).send(`Not found!`);
 };
