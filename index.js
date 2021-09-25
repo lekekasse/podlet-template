1f18b996fc9cc1e3026560df08fec1f17157888a
Trygve Lie <trygve.lie@finn.no>
2021-09-25 23:49:11 +0200

    Multiple exports

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ exports.helloWorld = (req, res) => {
   let message = req.query.message || req.body.message || 'Hello World!';
   res.status(200).send(message);
 };
+
+exports.content = (req, res) => {
+  let message = req.query.message || req.body.message || 'Hello content!';
+  res.status(200).send(message);
+};
+
+exports.fallback = (req, res) => {
+  let message = req.query.message || req.body.message || 'Hello fallback!';
+  res.status(200).send(message);
+};
