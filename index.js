3f5f9d7e6624d4410c92f3f3ffe254771f4e6396
Trygve Lie <trygve.lie@finn.no>
2021-09-26 12:19:31 +0200

    Test podium

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+import podlet from '@podium/podlet';
 import express from 'express';
 
 const app = express();
 
+const podlet = new Podlet({
+  name: 'myPodlet',
+  version: '1.0.0',
+  pathname: '/',
+  development: true,
+});
+
+app.use(podlet.middleware());
+
+app.get(podlet.content(), (req, res) => {
+  if (res.locals.podium.context.locale === 'nb-NO') {
+      return res.status(200).podiumSend('<h2>Hei verden</h2>');
+  }
+  res.status(200).podiumSend(`<h2>Hello world</h2>`);
+});
+
+app.get(podlet.manifest(), (req, res) => {
+  res.status(200).json(podlet);
+});
+
+
+/*
 app.get('/content', (req, res, next) => {
   res.send(`Hello content!`);
 });
+*/
 
 export { app as podlet };
 
