5de6719950eef0856cd92480029a48023db69cc9
Trygve Lie <trygve.lie@finn.no>
2021-09-26 16:24:20 +0200

    Use function

---
+++ function.js
@@ -0,0 +1,23 @@
+import Podlet from '@podium/podlet';
+import express from 'express';
+
+const app = express();
+
+const podlet = new Podlet({
+  name: 'myPodlet',
+  version: '1.0.0',
+  pathname: '/',
+  development: true,
+});
+
+app.use(podlet.middleware());
+
+app.get(podlet.content(), (req, res) => {
+  res.status(200).podiumSend(`<h2>Hello world</h2>`);
+});
+
+app.get(podlet.manifest(), (req, res) => {
+  res.status(200).json(podlet);
+});
+
+export { app as podletFn };
